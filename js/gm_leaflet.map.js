85cd65384a4a66c4538d6889515d35e3c9c4e088
Greg Sebastian <g.s.seabass@gmail.com>
2013-10-30 02:18:51 -0700

    Added support for layer switcher and multiple base layers

--- js/gm_leaflet.map.js
+++ js/gm_leaflet.map.js
@@ -9,20 +9,31 @@
       // Create Map
       var map = L.map(mapSettings.id, mapSettings.settings).setView([mapSettings.lat, mapSettings.lon], mapSettings.zoom);
 
+      var baseLayers = {};
+      var overlayLayers = {};
+
       // Add base layers
-      $.each(mapSettings['base layers'], function(name, layer){
+      $.each(mapSettings['base layers'], function(name, layerSettings){
         var layer;
-        if (layer.type == 'tile layer') {
-          layer = gmLeafletAddTileLayer(name, layer);
+        if (layerSettings.type == 'tile layer') {
+          layer = gmLeafletAddTileLayer(name, layerSettings);
         }
 
-        layer.addTo(map);
+        if (mapSettings.baseLayerCount == 1 || mapSettings['default base layer'] == name) {
+          layer.addTo(map);
+        }
+
+        if (mapSettings.baseLayerCount > 1) {
+          baseLayers[layerSettings.name] = layer;
+        }
       });
+
+      L.control.layers(baseLayers, overlayLayers).addTo(map);
     }
   };
 
-function gmLeafletAddTileLayer(name, layer) {
-  var layer = L.tileLayer(layer['url template'], layer.settings);
+function gmLeafletAddTileLayer(name, layerSettings) {
+  var layer = L.tileLayer(layerSettings['url template'], layerSettings.settings);
 
   return layer;
 }
